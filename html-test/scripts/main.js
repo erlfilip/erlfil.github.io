ceb1896e8d01d7491b71c61456c8e9e5ae2ab493
erlfilip <131300960+erlfilip@users.noreply.github.com>
2023-04-19 21:28:20 +0200

    Add files via upload

---
+++ html-test/scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick=()=>{
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/swb-logo.jpg") {
+        myImage.setAttribute("src", "images/gaspari.jpg")
+    } else {
+        myImage.setAttribute("src", "images/swb-logo.jpg");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if(!myName){
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.onclick=()=>{
+    setUserName();
+};
